c9cccbdc2fe63e845b5f5eb01d348609c6b8b5f1
Ash727 <32463254+Ash727@users.noreply.github.com>
2024-03-04 10:40:20 -0500

    Change in GetGnere Async function  and added example react project

--- FilmFinder-Code Cademy /public/script.js
+++ FilmFinder-Code Cademy /public/script.js
@@ -1,9 +1,6 @@
 const tmdbKey = '3a9b984588caccbda52935c3de27154b';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3/movie/11?api_key=';
 const playBtn = document.getElementById('playBtn');
-
-const getGenres = async () => {
- const genreRequestEndpoint = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
 const options = {
   method: 'GET',
   headers: {
@@ -12,6 +9,10 @@ const options = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTliOTg0NTg4Y2FjY2JkYTUyOTM1YzNkZTI3MTU0YiIsInN1YiI6IjY1ZDI4YmMzMTI0MjVjMDE2M2UzMmVjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NeeKC_ZXiZshAk84oMs9tcTejNRIFjyY0uLOP3zqw28'
   }
 };
+
+const getGenres = async () => {
+ const genreRequestEndpoint = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+  
   try {
     const response = await fetch(genreRequestEndpoint, options);
     if(response.ok){
@@ -29,6 +30,8 @@ const options = {
 
 const getMovies = () => {
   const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = 'https://api.themoviedb.org/3/discover/movie';
+  
 
 };
 
